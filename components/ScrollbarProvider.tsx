984301aea5bd33a4cba1cc28e4b7a0bb76b48462
lalkee <lalic.milan04@gmail.com>
2026-03-04 13:56:15 +0100

    Fixed scrollbar issue

--- components/ScrollbarProvider.tsx
+++ components/ScrollbarProvider.tsx
@@ -5,16 +5,11 @@ import 'simplebar-react/dist/simplebar.min.css';
 
 export default function RetroScrollProvider({ children }: { children: React.ReactNode }) {
   return (
-    <>
-      <div className="block md:hidden h-screen overflow-auto">
-        {children}
-      </div>
-
-      <div className="hidden md:block">
-        <SimpleBar style={{ height: '100vh' }} className="retro-scrollbar">
-          {children}
-        </SimpleBar>
-      </div>
-    </>
+    <SimpleBar 
+      style={{ height: '100vh' }} 
+      className="retro-scrollbar"
+    >
+      {children}
+    </SimpleBar>
   );
 }
